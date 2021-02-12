44fb908ace931f2b4c2a27b5c7616df5b7750623
tczkaraman <zehra.karaman@turkcell.com.tr>
2021-02-13 01:38:08 +0300

    potd

--- starter/app/src/main/java/com/udacity/asteroidradar/BindingAdapters.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/BindingAdapters.kt
@@ -2,7 +2,9 @@ package com.udacity.asteroidradar
 
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
+import com.squareup.picasso.Picasso
 
 @BindingAdapter("statusIcon")
 fun bindAsteroidStatusImage(imageView: ImageView, isHazardous: Boolean) {
@@ -38,4 +40,13 @@ fun bindTextViewToKmUnit(textView: TextView, number: Double) {
 fun bindTextViewToDisplayVelocity(textView: TextView, number: Double) {
     val context = textView.context
     textView.text = String.format(context.getString(R.string.km_s_unit_format), number)
+}
+
+@BindingAdapter("imageUrl")
+fun bindImage(imgView: ImageView, imgUrl: String?) {
+    imgUrl?.let {
+        val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
+        val context = imgView.context
+        Picasso.with(context).load(imgUri).into(imgView);
+    }
 }
